5f0784a5e8227a6f44690efb46d738fb026c6d9d
Lucas Matos <lucaspmatos@outlook.com>
2020-02-11 15:20:45 -0300

    Pop Ball Game Files

---
+++ script.js
@@ -0,0 +1,20 @@
+function addBall() {
+    var ball = document.createElement("div");
+    ball.setAttribute("class", "ball");
+
+    var p1 = Math.floor(Math.random() * 500);
+    var p2 = Math.floor(Math.random() * 400);
+
+    ball.setAttribute("style", "left:"+p1+"px;top:"+p2+"px;");
+    ball.setAttribute("onclick", "popBall(this)");
+
+    document.body.appendChild(ball);
+}
+
+function popBall(object) {
+    document.body.removeChild(object);
+}
+
+function startGame() {
+    setInterval(addBall, 1000);
+}
